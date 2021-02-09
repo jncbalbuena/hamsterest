1850472b9a8190329100648a63262b551e7a927c
GeneRoca <52713912+GeneRoca@users.noreply.github.com>
2021-02-10 00:44:00 +0800

    created different pages

---
+++ hamsterest-about.js
@@ -0,0 +1,18 @@
+var counter = 1;
+
+setInterval(function(){
+	document.getElementById('radio' + counter).checked = true;
+	counter++;
+	if(counter > 4){
+		counter = 1;
+	}
+}, 5000);
+
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+})
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("nav");
+	header.classList.toggle("sticky", window.scrollY > 0);
+})
